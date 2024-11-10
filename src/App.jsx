963bd7f051e91cfbac6a3732f04523362933cc78
B <apple@MacBook-Pro.local>
2024-11-10 14:01:04 +0100

    about, magic and titles updated

--- src/App.jsx
+++ src/App.jsx
@@ -164,11 +164,11 @@ function App() {
         <BurgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} onNavigate={scrollToSection} />
       </header>
       <div className="about">
-        <div className="title">
-          <h1>MASSA CONFUSA</h1>
-        </div>
         <div className="description">
           <h3>MASSA CONFUSA IS A LOS ANGELES BASED CREATIVE LABORATORY EXPLORING THE SUBTLE ART OF TRANSFORMATION. HERE, CREATIVE CHAOS ISN’T SOMETHING TO CONTROL—IT’S A CATALYST FOR DISCOVERY.</h3>
+        </div>
+        <div className="title">
+          <h1>MASSA CONFUSA</h1>
         </div>
       </div>
       <div className="explore">
