02b21d70c3d5409e4b4c580635ddcb322578c290
B <apple@MacBook-Pro.local>
2024-11-10 23:02:54 +0100

    intro card responsiveness

--- src/components/cards/IntroBookCard/IntroBookCard.jsx
+++ src/components/cards/IntroBookCard/IntroBookCard.jsx
@@ -11,7 +11,7 @@ const IntroBookCard = ({ moveCards }) => {
                 <h2 className="intro-book-card-title">Explore the Lab</h2>
                 <div className="intro-book-card-paragraph-button">
                 <p className="intro-book-card-paragraph">OUR SYSTEM BLENDS PSYCHOLOGY, MEDITATIVE PRACTICES, AND THE ART OF PERFORMANCE INTO A SINGLE TRANSFORMATIVE EXPERIENCE.</p>
-                <p className="intro-book-card-paragraph">WE DIVE DEEP, HELPING YOU EXPLORE HIDDEN PARTS OF YOURSELF, BUILD SELF-CONFIDENCE, AND UNCOVER YOUR POTENTIAL THROUGH FOUR STAGES IN A CYCLE OF CONTINUOUS GROWTH.</p>
+                <p className="intro-book-card-paragraph-two">WE DIVE DEEP, HELPING YOU EXPLORE HIDDEN PARTS OF YOURSELF, BUILD SELF-CONFIDENCE, AND UNCOVER YOUR POTENTIAL THROUGH FOUR STAGES IN A CYCLE OF CONTINUOUS GROWTH.</p>
                 <CustomButton
                     buttonData={{ type: 'icon button', icon: arrow }}
                     scrollAction={moveCards}
