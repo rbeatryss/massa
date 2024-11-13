6877afd6cb8a092379003856a54f802bda2434de
B <apple@MacBook-Pro.local>
2024-11-14 00:15:57 +0100

    buttons deselect on scroll/other picked

--- src/components/buttons/CustomButton.jsx
+++ src/components/buttons/CustomButton.jsx
@@ -1,62 +1,54 @@
+// src/components/buttons/CustomButton.js
 import React, { useState, useEffect } from 'react';
 import './CustomButton.css';
+import EventButton from './eventButton';
 
-const CustomButton = ({ buttonData, link, scrollAction }) => {
-    const { type, eventName, eventDate, eventTime, label, icon } = buttonData;
 
-    const buttonClass = type === 'coming soon' ? 'coming-soon-button' 
-                    : type === 'event button' ? 'event-button' 
-                    : type === 'small button' ? 'small-button'
-                    : type === 'icon button' ? 'icon-button'
-                    : 'action-button';
+const CustomButton = ({ buttonData, link, scrollAction, isSelected, onButtonClick, index }) => {
+  const { type, eventName, eventDate, eventTime, label, icon } = buttonData;
 
-    const [clicked, setClicked] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
+  const buttonClass =
+    type === 'coming soon'
+      ? 'coming-soon-button'
+      : type === 'event button'
+      ? 'event-button'
+      : type === 'small button'
+      ? 'small-button'
+      : type === 'icon button'
+      ? 'icon-button'
+      : 'action-button';
 
-    useEffect(() => {
-        setIsMobile(window.innerWidth <= 768);
-    }, []);
+  const [clicked, setClicked] = useState(isSelected); // Set initial state based on selection
 
-    const handleClick = () => {
-        if (scrollAction) {
-            scrollAction();
-        } else {
-            if (isMobile) {
-                if (clicked) {
-                    window.location.href = link;
-                } else {
-                    setClicked(true);
-                }
-            } else {
-                window.location.href = link;
-            }
-        }
-    };
+  useEffect(() => {
+    setClicked(isSelected); // Update state when the button's selection status changes
+  }, [isSelected]);
 
-    return (
-        <button 
-            className={`custom-button ${buttonClass} ${clicked && isMobile ? 'bright' : ''}`} 
-            onClick={handleClick}
-        >
-            {type === 'event button' && eventName && eventDate && eventTime ? (
-                <div className="event-content">
-                <div className="event-details">
-                    <span className="event-date">{eventDate}</span>
-                    <span className="event-time">{eventTime}</span>
-                </div>
-                <div className="event-name-container">
-                    <span className="event-name">{eventName}</span>
-                </div>
-            </div>
-        ) : type === 'icon button' && icon ? (
-            <span className="icon"> 
-                <img src={icon} alt="icon" />
-            </span> 
-        ) : (
-            <span>{label}</span>
-        )}
-        </button>
-    );
+  const handleClick = () => {
+    if (scrollAction) {
+      scrollAction();
+    } else {
+      setClicked(true);
+      onButtonClick(index); 
+    }
+  };
+
+  return (
+    <button
+      className={`custom-button ${buttonClass} ${clicked ? 'bright' : ''}`}
+      onClick={handleClick}
+    >
+      {type === 'event button' && eventName && eventDate && eventTime ? (
+        <EventButton eventName={eventName} eventDate={eventDate} eventTime={eventTime} />
+      ) : type === 'icon button' && icon ? (
+        <span className="icon">
+          <img src={icon} alt="icon" />
+        </span>
+      ) : (
+        <span>{label}</span>
+      )}
+    </button>
+  );
 };
 
 export default CustomButton;
--- src/components/cards/bookCard/BookCard.jsx
+++ src/components/cards/bookCard/BookCard.jsx
@@ -1,36 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './BookCard.css';
 import CustomButton from '../../buttons/CustomButton';
 
+const BookCard = ({
+  title,
+  descriptor,
+  description,
+  mobileDescription,
+  desktopDescription,
+  gradient,
+  shadow,
+  buttons,
+  overlayColor,
+  backgroundImage,
+}) => {
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
+  const cardRef = React.useRef(null); // Reference for the current card
 
-const BookCard = ({ title, descriptor, description, mobileDescription, desktopDescription, gradient, shadow, buttons, overlayColor, backgroundImage, }) => {
+  // Function to handle button click
+  const handleButtonClick = (index) => {
+    setSelectedButtonIndex(index);
+  };
 
-    return (
-        <div className="book-card"
-            style={{
-                background: gradient,
-                boxShadow: shadow,
-                '--overlay-color': overlayColor,
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}>
-            <div className="book-card-content-container">
-            <div className="book-card-header">
-                <h2 className="book-card-title">{title}</h2>
-                <h3 className="book-card-descriptor">{descriptor}</h3>
-                </div>
-                <p className=".book-card-description">{description}</p>
-                <p className=".book-card-description-mobile">{mobileDescription}</p>
-                <p className=".book-card-description-desktop">{desktopDescription}</p>
-            </div>
-            <div className="book-card-button-container">
-                {buttons.map((buttonData, index) => (
-                    <CustomButton key={index} buttonData={buttonData} />
-                ))}
-            </div>
-        </div>
+  // Set up the IntersectionObserver
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setSelectedButtonIndex(); 
+          }
+        });
+      },
+      { threshold: 0.5 }
     );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={cardRef}
+      className="book-card"
+      style={{
+        background: gradient,
+        boxShadow: shadow,
+        '--overlay-color': overlayColor,
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="book-card-content-container">
+        <div className="book-card-header">
+          <h2 className="book-card-title">{title}</h2>
+          <h3 className="book-card-descriptor">{descriptor}</h3>
+        </div>
+        <p className="book-card-description">{description}</p>
+        <p className="book-card-description-mobile">{mobileDescription}</p>
+        <p className="book-card-description-desktop">{desktopDescription}</p>
+      </div>
+      <div className="book-card-button-container">
+        {buttons.map((buttonData, index) => (
+          <CustomButton
+            key={index}
+            buttonData={buttonData}
+            index={index}
+            isSelected={selectedButtonIndex === index}
+            onButtonClick={handleButtonClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BookCard;
